d9129ecb8ab7800590b7eb376ee19edf1a743e90
LaeekAhmed <laeek385@gmail.com>
2024-06-09 14:06:02 -0400

    chore: Update dependencies and remove unused code

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import { Footer } from "flowbite-react";
-import {
-   BsDribbble,
-   BsFacebook,
-   BsGithub,
-   BsGlobe,
-   BsInstagram,
-   BsLinkedin,
-   BsTwitter,
-} from "react-icons/bs";
+import { BsGithub, BsGlobe, BsLinkedin } from "react-icons/bs";
 
 export default function FooterWithSocialMediaIcons() {
    const handleScroll = () => {
@@ -27,41 +19,19 @@ export default function FooterWithSocialMediaIcons() {
                   disabled={false}
                   type="button"
                   onClick={handleScroll}
-                  className="m-1 inline-flex font-semibold items-center px-5 py-2 bg-transparent text-black text-base no-underline border-2 border-black rounded-lg transition duration-300 hover:bg-indigo-600 hover:border-white hover:text-white"
-               >
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     strokeWidth={1.5}
-                     stroke="currentColor"
-                     className="w-6 h-6"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
-                     />
+                  className="m-1 inline-flex font-semibold items-center px-5 py-2 bg-transparent text-black text-base no-underline border-2 border-black rounded-lg transition duration-300 hover:bg-indigo-600 hover:border-white hover:text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
                   </svg>
                </button>
             </div>
 
             <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
                <div className="flex items-center">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     viewBox="0 0 24 24"
-                     strokeWidth={1.5}
-                     stroke="currentColor"
-                     className="w-10 h-10 mr-3"
-                  >
-                     <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                     />
-                  </svg> <span className="text-2xl font-bold">Flypedia</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 mr-3">
+                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                  </svg>{" "}
+                  <span className="text-2xl font-bold">Flypedia</span>
                </div>
                <div className="">
                   <div>
@@ -89,7 +59,7 @@ export default function FooterWithSocialMediaIcons() {
             </div>
             <Footer.Divider />
             <div className="w-full sm:flex sm:items-center sm:justify-between">
-               <Footer.Copyright by="Made with Next.js" href="https://nextjs.org/"/>
+               <Footer.Copyright by="Made with Next.js" href="https://nextjs.org/" />
 
                <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
                   <Footer.Icon target="_blank" rel="noopener" href="https://www.linkedin.com/in/laeek-ahmed-shaikh/" icon={BsLinkedin} />
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { Fragment, useEffect, useState } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 interface ele {
-  name : string,
-  href : string,
-  current : boolean,
-  ext : boolean
-} 
+   name: string;
+   href: string;
+   current: boolean;
+   ext: boolean;
+}
 
-const navigation : ele[] = [
+const navigation: ele[] = [
+   { name: "Home", href: "/", current: false, ext: false },
    { name: "Airlines", href: "/airlines", current: true, ext: false },
-   { name: "Home", href: "/", current: false, ext:false },
    { name: "GitHub", href: "https://github.com/LaeekAhmed/flypedia", current: false, ext: true },
    { name: "Analytics", href: "https://analytics.umami.is/share/FQL7KD5bhI2SRfIe/flypedia", current: false, ext: true },
 ];
@@ -22,19 +23,18 @@ function classNames(...classes: string[]) {
 }
 
 export default function Example() {
-   
    const [updatedNavigation, setUpdatedNavigation] = useState<ele[]>(navigation);
 
    useEffect(() => {
-     const currentPath = window.location.pathname;
- 
-     // Clone the original navigation array and update the current property
-     const updatedNav = navigation.map((item) => ({
-       ...item,
-       current: item.href === currentPath,
-     }));
- 
-     setUpdatedNavigation(updatedNav);
+      const currentPath = window.location.pathname;
+
+      // Clone the original navigation array and update the current property
+      const updatedNav = navigation.map((item) => ({
+         ...item,
+         current: item.href === currentPath,
+      }));
+
+      setUpdatedNavigation(updatedNav);
    }, []);
 
    return (
@@ -47,37 +47,16 @@ export default function Example() {
                         {/* Mobile menu button*/}
                         <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                            <span className="sr-only">Open main menu</span>
-                           {open ? (
-                              <XMarkIcon
-                                 className="block h-6 w-6"
-                                 aria-hidden="true"
-                              />
-                           ) : (
-                              <Bars3Icon
-                                 className="block h-6 w-6"
-                                 aria-hidden="true"
-                              />
-                           )}
+                           {open ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
                         </Disclosure.Button>
                      </div>
                      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex flex-shrink-0 items-center">
-                          <a href="/">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-8 h-8"
-                            >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                                />
-                            </svg>
-                          </a>
+                           <Link href="/">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                              </svg>
+                           </Link>
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                            <div className="flex space-x-4">
@@ -85,35 +64,20 @@ export default function Example() {
                                  <a
                                     key={item.name}
                                     href={item.href}
-                                    target={item.ext ? '_blank' : '_self'}
-                                    rel={item.ext ? 'noopener' : undefined}
-                                    className={classNames(
-                                       item.current
-                                          ? "bg-gray-900 text-white"
-                                          : "text-black hover:bg-indigo-600 hover:text-white",
-                                       "rounded-md px-3 py-2 text-sm font-medium"
+                                    target={item.ext ? "_blank" : "_self"}
+                                    rel={item.ext ? "noopener" : undefined}
+                                    className={classNames(item.current ? "bg-gray-900 text-white" : "text-black hover:bg-indigo-600 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")}
+                                    aria-current={item.current ? "page" : undefined}>
+                                    {item.ext ? (
+                                       <>
+                                          {item.name}
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 inline-block ml-2">
+                                             <path d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                          </svg>
+                                       </>
+                                    ) : (
+                                       item.name
                                     )}
-                                    aria-current={
-                                       item.current ? "page" : undefined
-                                    }
-                                 >
-                                  {item.ext ? (
-                                    <>
-                                      {item.name}
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-5 h-5 inline-block ml-2"
-                                      >
-                                        <path d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                                      </svg>
-                                    </>
-                                  ) : (
-                                    item.name
-                                  )}
                                  </a>
                               ))}
                            </div>
@@ -129,14 +93,8 @@ export default function Example() {
                            key={item.name}
                            as="a"
                            href={item.href}
-                           className={classNames(
-                              item.current
-                                 ? "bg-gray-900 text-white"
-                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "block rounded-md px-3 py-2 text-base font-medium"
-                           )}
-                           aria-current={item.current ? "page" : undefined}
-                        >
+                           className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block rounded-md px-3 py-2 text-base font-medium")}
+                           aria-current={item.current ? "page" : undefined}>
                            {item.name}
                         </Disclosure.Button>
                      ))}
